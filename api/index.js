77217a25cda05851d4a0409fbef0d96a35756b15
Pavitra-jain-303 <pavitrajain303@gmail.com>
2023-09-29 00:08:25 +0530

    c

--- api/index.js
+++ api/index.js
@@ -37,7 +37,7 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", catRoute);
 
-
-app.listen("5000", () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     console.log("Backend is running at PORT 5000");
 })
